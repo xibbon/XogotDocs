cfd4167d978b319b8460d83dad3ed98d81757861
Nicolas St-Amour <nstamour.dev@outlook.com>
2026-07-26 13:49:00 -0400

    fix: flatten player Y position when initializing mobs to prevent tilting

--- Documentation.docc/Resources/code-files/your-first-3d-game/main-3d-02.gd
+++ Documentation.docc/Resources/code-files/your-first-3d-game/main-3d-02.gd
@@ -13,7 +13,7 @@ func _on_mob_timer_timeout():
     # And give it a random offset.
     mob_spawn_location.progress_ratio = randf()
 
-    var player_position = $Player.position
+    var player_position = Vector3($Player.position.x, 0, $Player.position.z)
     mob.initialize(mob_spawn_location.position, player_position)
 
     # Spawn the mob by adding it to the Main scene.
